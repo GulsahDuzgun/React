5248009ea2e707dcc40f3111304f1bbda7c26f1d
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-04-18 16:31:05 +0300

    initial commit :turtle:

---
+++ 04 - Travel List/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
