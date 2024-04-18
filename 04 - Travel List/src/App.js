ffce0ff4822c93baf7d111595dc016aef43f7ac8
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-04-18 17:16:57 +0300

    Rendering the Items List

--- 04 - Travel List/src/App.js
+++ 04 - Travel List/src/App.js
@@ -1,12 +1,61 @@
-import "./App.css";
+import "./index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
 ];
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item itemData={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ itemData }) {
+  return (
+    <li>
+      <span style={itemData.packed ? { textDecoration: "line-through" } : {}}>
+        {itemData.quantity} {itemData.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
