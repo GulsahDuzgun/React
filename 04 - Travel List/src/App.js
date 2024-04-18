8fefc8b5e768e42596aabad819e5dadd46198d2b
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-04-18 18:38:49 +0300

    Controlled Elements

--- 04 - Travel List/src/App.js
+++ 04 - Travel List/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const initialItems = [
@@ -22,21 +23,50 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return; //guard
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    setDescription("");
+    setQuantity(1);
+
+    initialItems.push(newItem);
+    console.log(newItem);
+    console.log(initialItems);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (item, indx) => (
           <option key={indx} value={indx + 1}>
             {indx + 1}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
