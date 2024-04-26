84443595597a07a073d870fd3af56442b4f0384c
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-04-26 15:30:32 +0300

    How not to Fetch Data in React

--- 06 - usePopcorn/src/index.js
+++ 06 - usePopcorn/src/index.js
@@ -1,28 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [tRate, setTRate] = useState(0);
-  return (
-    <div>
-      <StarRating onSetTRate={setTRate} maxRating={7} color="blue" />
-      <p>The movie was rated {tRate}</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App2";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={48}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <Test></Test>
+    <App />
   </React.StrictMode>
 );
