872b3b460a1a430530317a912f170823fdef59b5
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-07 15:02:14 +0300

    Building the Pages

--- 09 - WorldWise/src/components/PageNav.jsx
+++ 09 - WorldWise/src/components/PageNav.jsx
@@ -1,21 +1,25 @@
 import { NavLink } from "react-router-dom";
 import style from "./PageNav.module.css";
+import Logo from "./Logo";
 
 export default function PageNav() {
   return (
     <nav className={style.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/*">Not Found</NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink href="/">Home</NavLink>
+          <NavLink to="/login" className={style.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
---
+++ 09 - WorldWise/src/pages/AppLayout.jsx
@@ -0,0 +1,5 @@
+import style from "./AppLayout.module.css";
+
+export default function AppLayout() {
+  return <nav className={style.app}>AppLayout</nav>;
+}
