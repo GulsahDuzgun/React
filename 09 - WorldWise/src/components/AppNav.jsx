cd18163d5873086b70c0ce9409c2952b025dd59c
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-07 13:54:18 +0300

    Using CSS Modules

---
+++ 09 - WorldWise/src/components/AppNav.jsx
@@ -0,0 +1,5 @@
+import style from "./AppNav.module.css";
+
+export default function AppNav() {
+  return <nav className={style.nav}>APP NAVIGATION</nav>;
+}
---
+++ 09 - WorldWise/src/pages/PageLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "../components/AppNav";
+
+export default function PageLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>Layout</p>
+    </div>
+  );
+}
