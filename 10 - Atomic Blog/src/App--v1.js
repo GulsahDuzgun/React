c4090503bb75b67bbe71a8f6501e4d6a0d6e5731
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-09 16:48:11 +0300

    Advanced Pattern: A Custom Provider

--- 10 - Atomic Blog/src/App--v1.js
+++ 10 - Atomic Blog/src/App--v1.js
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { PostContext, PostProvider } from "./PostProvider";
+import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -8,9 +7,32 @@ function createRandomPost() {
     body: faker.hacker.phrase(),
   };
 }
+const PostContext = createContext();
 
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -21,8 +43,16 @@ function App() {
   );
 
   return (
-    <section>
-      <PostProvider>
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
         <button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
           className="btn-fake-dark-mode"
@@ -34,8 +64,8 @@ function App() {
         <Main />
         <Archive />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   );
 }
 
