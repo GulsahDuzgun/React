e34229c0164fc73207ff546c023cfe5f8dc12dec
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-09 23:04:34 +0300

    Including a Map With the Leaflet Library

--- 09 - WorldWise/src/components/Map.jsx
+++ 09 - WorldWise/src/components/Map.jsx
@@ -1,20 +1,31 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 
 function Map() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [mapPosition] = useState([42, 0]);
+  // const [searchParams] = useSearchParams();
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
-      <button onClick={() => setSearchParams({ lat: 12, lng: 27 })}>
-        Change
-      </button>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWhellZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>Popup</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
