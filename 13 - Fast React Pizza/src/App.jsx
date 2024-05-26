99eae4abe14c73fb47e61574a3dc9d18f9473c31
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-26 08:49:10 +0300

    A new way of implementing Routes

--- 13 - Fast React Pizza/src/App.jsx
+++ 13 - Fast React Pizza/src/App.jsx
@@ -1,3 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  { path: "/menu", element: <Menu /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 export default function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
