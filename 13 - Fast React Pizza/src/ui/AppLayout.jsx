001d8fb8ad62398a08b01f5e435657be88eecd36
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-27 08:59:11 +0300

    displaying a loading indicator

--- 13 - Fast React Pizza/src/ui/AppLayout.jsx
+++ 13 - Fast React Pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const { state: isLoading } = useNavigation();
+
+  {
+    isLoading === "loading" && <Loader />;
+  }
   return (
-    <div>
+    <div className="layout">
       <Header />
       <main>
         <Outlet />
