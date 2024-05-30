ab2b9c6d2a9d04c530729e8131b2074cf4ef3eaa
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-30 14:09:18 +0300

    Styling Text

--- 13 - Fast React Pizza/src/ui/Header.jsx
+++ 13 - Fast React Pizza/src/ui/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/searchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500">
+    <header className="bg-yellow-500 uppercase">
       <h2>Hello Gülşah? Keep going!!</h2>
       <p>Header</p>
-      <Link to="/">Home</Link>
+      <Link className="tracking-widest" to="/">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
