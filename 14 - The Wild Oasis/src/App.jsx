200e5976a7cf618b56b819e412abb544254344b3
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-09 19:34:59 +0300

    Setting Up PAges and Routes

--- 14 - The Wild Oasis/src/App.jsx
+++ 14 - The Wild Oasis/src/App.jsx
@@ -1,34 +1,27 @@
-import styled from "styled-components";
-import GlobalStyle from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Cabins from "./pages/Cabins";
+import Bookings from "./pages/Bookings";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 
 export default function App() {
-  const StyledApp = styled.main`
-    padding: 10px;
-  `;
-
   return (
-    <>
-      <GlobalStyle />
-      <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">Keep Going</Heading>
-          <Heading as="h2">Forward</Heading>
-        </Row>
-        <Row>
-          <Button>There we go</Button>
-          <Button variation="danger" size="large">
-            No Pain No Gain
-          </Button>
-          <Button variation="secondary" size="medium">
-            Move on{" "}
-          </Button>
-          <Input placeholder="Hello World" />
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="account" element={<Account />}></Route>
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="login" element={<Login />} />
+        <Route path="user" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
