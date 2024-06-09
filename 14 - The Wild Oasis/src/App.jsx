6774903cc30a6f83bbb8c75bce38155114801657
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-09 15:34:46 +0300

    Global Styles With Styled Components

--- 14 - The Wild Oasis/src/App.jsx
+++ 14 - The Wild Oasis/src/App.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 export default function App() {
   const H1 = styled.h1`
@@ -7,39 +10,20 @@ export default function App() {
     background-color: rebeccapurple;
   `;
 
-  const Button = styled.button`
-    border: none;
-    padding: 1.2rem 2.4rem;
-    background-color: orangered;
-    color: white;
-    font-size: 1.8rem;
-    margin: 0.8rem;
-    border-radius: 8px;
-  `;
-
-  const Input = styled.input`
-    border-radius: 1.2rem;
-    padding: 1.2rem 2.4rem;
-    font-size: 1.8rem;
-    &::placeholder-color {
-      color: transparent;
-    }
-    &::-webkit-input-placeholder {
-      color: transparent;
-    }
-  `;
-
   const StyledApp = styled.main`
     background-color: darkblue;
     padding: 10px;
   `;
 
   return (
-    <StyledApp>
-      <H1>Keep Going</H1>
-      <Button>There we go</Button>
-      <Button onClick={() => alert("Hello")}>No Pain No Gain</Button>
-      <Input placeholder="Hello World" />
-    </StyledApp>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>Keep Going</H1>
+        <Button>There we go</Button>
+        <Button onClick={() => alert("Hello")}>No Pain No Gain</Button>
+        <Input placeholder="Hello World" />
+      </StyledApp>
+    </>
   );
 }
