e8513c81c6a380484b23a81fb50cfbb7eeafc6d8
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-09 19:53:00 +0300

    execute the Global Component Style

--- 14 - The Wild Oasis/src/App.jsx
+++ 14 - The Wild Oasis/src/App.jsx
@@ -7,21 +7,25 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyles";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route index element={<Navigate to="dashboard" replace />} />
-        <Route path="account" element={<Account />}></Route>
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="login" element={<Login />} />
-        <Route path="user" element={<Users />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="account" element={<Account />}></Route>
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="login" element={<Login />} />
+          <Route path="user" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
