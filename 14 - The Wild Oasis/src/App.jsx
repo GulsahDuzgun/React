af848670bfece6bde7634dd54306ca0281432a7f
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-09 17:23:13 +0300

    Building More Reusable Styled Component

--- 14 - The Wild Oasis/src/App.jsx
+++ 14 - The Wild Oasis/src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyle from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 export default function App() {
   const StyledApp = styled.main`
@@ -13,11 +14,20 @@ export default function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <Heading as="h1">Keep Going</Heading>
-        <Heading as="h2">Forward</Heading>
-        <Button>There we go</Button>
-        <Button onClick={() => alert("Hello")}>No Pain No Gain</Button>
-        <Input placeholder="Hello World" />
+        <Row type="horizontal">
+          <Heading as="h1">Keep Going</Heading>
+          <Heading as="h2">Forward</Heading>
+        </Row>
+        <Row>
+          <Button>There we go</Button>
+          <Button variation="danger" size="large">
+            No Pain No Gain
+          </Button>
+          <Button variation="secondary" size="medium">
+            Move on{" "}
+          </Button>
+          <Input placeholder="Hello World" />
+        </Row>
       </StyledApp>
     </>
   );
