5e246dcbb9b35b49474c3862c889db6641b127d5
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-19 10:33:49 +0300

    API-Side Sorting: Sorting Bookings

--- 14 - The Wild Oasis/src/features/bookings/useBookings.js
+++ 14 - The Wild Oasis/src/features/bookings/useBookings.js
@@ -5,15 +5,19 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
   const filterVal = searchParams.get("status");
+  const sortByVal = searchParams.get("sortBy") || "startDate-desc";
 
   const filter =
     !filterVal || filterVal === "all"
       ? null
       : { field: "status", value: filterVal, method: "eq" };
 
+  const [field, direction] = sortByVal.split("-");
+  const sortBy = !sortByVal ? null : { field, direction };
+
   const { data: bookings, isLoading } = useQuery({
-    queryFn: () => getBookings({ filter }),
-    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy],
   });
 
   return { bookings, isLoading };
