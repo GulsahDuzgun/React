2cfe8928d6ec242fb3d56ccd5ab475bb3175af9c
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-19 13:55:09 +0300

    prefetching with React Query

--- 14 - The Wild Oasis/src/features/bookings/useBookings.js
+++ 14 - The Wild Oasis/src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { BOOKINGS_PER_PAGE } from "../../utils/constants";
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
   const filterVal = searchParams.get("status");
   const sortByVal = searchParams.get("sortBy") || "startDate-desc";
 
@@ -23,6 +25,24 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, currentPage }),
     queryKey: ["bookings", filter, sortBy, currentPage],
   });
+
+  const countPage = Math.ceil(count / BOOKINGS_PER_PAGE);
+
+  if (currentPage <= countPage) {
+    queryClient.prefetchQuery({
+      queryFn: () =>
+        getBookings({ filter, sortBy, currentPage: currentPage + 1 }),
+      queryKey: ["bookings", filter, sortBy, currentPage + 1],
+    });
+  }
+
+  if (currentPage > 1) {
+    queryClient.prefetchQuery({
+      queryFn: () =>
+        getBookings({ filter, sortBy, currentPage: currentPage - 1 }),
+      queryKey: ["bookings", filter, sortBy, currentPage - 1],
+    });
+  }
 
   return { bookings, isLoading, count };
 }
