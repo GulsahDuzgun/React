a3dd5e45f6d4e2f9b271e55750bc4a23756d08ae
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-24 19:16:03 +0300

    Displaying a Pie Chart

--- 14 - The Wild Oasis/src/features/dashboard/DashboardLayout.jsx
+++ 14 - The Wild Oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "../bookings/useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -31,6 +32,7 @@ export default function DashboardLayout() {
         allCabins={cabins.length}
         numDays={numDays}
       />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart numDays={numDays} bookings={allBookings} />
     </StyledDashboardLayout>
   );
