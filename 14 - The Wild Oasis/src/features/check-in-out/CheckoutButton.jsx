0430cf4ed7bee2814c924fd3d3210244aea79906
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-25 15:48:53 +0300

    Displaying Stays for Current Day

--- 14 - The Wild Oasis/src/features/check-in-out/CheckoutButton.jsx
+++ 14 - The Wild Oasis/src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,17 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkOutLoading, checkOutFnc } = useCheckout();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => {
+        checkOutFnc(bookingId);
+      }}
+      disabled={checkOutLoading}
+    >
       Check out
     </Button>
   );
--- 14 - The Wild Oasis/src/features/dashboard/DashboardLayout.jsx
+++ 14 - The Wild Oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -32,6 +33,7 @@ export default function DashboardLayout() {
         allCabins={cabins.length}
         numDays={numDays}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart numDays={numDays} bookings={allBookings} />
     </StyledDashboardLayout>
